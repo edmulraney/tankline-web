ddc0e19a49980fd8ccf229db17e803b2aa7c88bc
Edward Mulraney <edmulraney@gmail.com>
2020-02-22 21:15:15 +0000

    enable pack and publish

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,7 +2,7 @@
 
 package="packages/$1"
 if [ -d $package ]; then
-  cd $package && npm pack
+  cd $package && npm run pack
 else
   echo "package $1 doesn't exist"
   exit 1
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,12 +2,7 @@
 
 package="packages/$1"
 if [ -d $package ]; then
-  artifact=$(find $package -name "$1-*.tgz")
-  if [ -z $artifact ]; then
-    echo "you need to pack $1 first: npm run pack $1"
-    exit 1
-  fi
-  npm publish $artifact
+  cd $package && npm run publish
 else
   echo "package $1 doesn't exist"
   exit 1
